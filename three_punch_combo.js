b2fbf8441e3318fab8fb89a349594097e8ccad84
liuzhifei <2679431923@qq.com>
2021-11-01 14:30:20 +0800

    add more gift

--- three_punch_combo.js
+++ three_punch_combo.js
@@ -1,7 +1,7 @@
         $i = 0;
         $('#start').click(function(){
             $i++;
-            if($i >=6 ){
+            if($i >=16 ){
                 $('#start').hide();
                 $('#stop').show();
             }
